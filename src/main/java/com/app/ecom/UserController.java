0172e15dbc1259215b576cca59f0c857fec6d3c1
Sonu Kumar <sonusaini48292@gmail.com>
2026-06-30 14:00:08 +0530

    added the user get by id with modern stream api code

--- src/main/java/com/app/ecom/UserController.java
+++ src/main/java/com/app/ecom/UserController.java
@@ -2,26 +2,38 @@ package com.app.ecom;
 
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class UserController {
-    private List<User> userList = new ArrayList<>();
+
+
+    private final UserService userService;
+
 
     @GetMapping("/api/users")
-    public List<User> getAllUsers() {
-        return userList;
+    public ResponseEntity<List<User>> getAllUsers() {
+        return new ResponseEntity<>(userService.fetchAllUsers(), HttpStatus.OK);
+    }
+
+    @GetMapping("/api/users/{id}")
+    public ResponseEntity<User> getUser(@PathVariable Long id) {
+        return userService.fetchUser(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping("/api/users")
-    public List<User> createUser(@RequestBody User user) {
-        userList.add(user);
-        return userList;
+    public ResponseEntity<String> createUser(@RequestBody User user) {
+        userService.addUser(user);
+        return ResponseEntity.ok("User added successfully");
     }
 }
--- src/main/java/com/app/ecom/UserService.java
+++ src/main/java/com/app/ecom/UserService.java
@@ -5,17 +5,27 @@ import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
     private List<User> userList = new ArrayList<>();
+    private Long nextId = 1L;
 
     public List<User> fetchAllUsers() {
         return userList;
     }
 
     public List<User> addUser(User user) {
+        user.setId(nextId++);
         userList.add(user);
         return userList;
+    }
+
+    public Optional<User> fetchUser(Long id) {
+        return userList.stream()
+                .filter(user -> user.getId().equals(id))
+                .findFirst();
+
     }
 }
